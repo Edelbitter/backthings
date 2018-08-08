620d6338f39d97808907ae2a275b474a46cc9f6b
Regine Gorges <gorges.regine@gmail.com>
2018-08-08 12:24:43 +0200

    cors shit

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const router = express.Router();
 const port = process.env.PORT || 3000;
 const url = require('url');
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.get('/', (request, response) => response.send('Hiho'));
 
 app.use('/api', router);
