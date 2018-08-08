8e936f4eea4cf97a09408e48eabf094181eebff6
Regine Gorges <gorges.regine@gmail.com>
2018-08-08 16:38:32 +0200

    save subscriptions and send notifications

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const app = express();
 const router = express.Router();
 const port = process.env.PORT || 3000;
 const url = require('url');
+const webpush = require('web-push');
+
+const vapidKeys = {
+    "publicKey": "BAQsFOEQGlL7T08PGFDSLcsYXrbfBndZjx8ZTtmLsFbNdbpW06mxvdjX6P7jgTJWmihBHrQ3cFFOdzHjrbP6RI8",
+    "privateKey": "18bEjco9FJsVTDt2dIZbtB3zx32LnRIpviqk04i3iZ8"
+}
+
+let subscriptions = [];
+
+webpush.setVapidDetails(
+    'mailto:example@yourdomain.org',
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+);
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
@@ -33,5 +47,36 @@ router.get('/things', (request, response) => {
 });
 
 app.post('/', (req, res) => {
+    subscriptions.push(req.body);
     res.json({ yo: "hoho" });
 })
+
+app.post('/send',sendNotification)
+
+export function sendNotification(req,res) {
+    const notificationPayload = {
+        "notification": {
+            "title": "Angular News",
+            "body": "Newsletter Available!",
+            "icon": "assets/main-page-logo-small-hat.png",
+            "vibrate": [100, 50, 100],
+            "data": {
+                "dateOfArrival": Date.now(),
+                "primaryKey": 1
+            },
+            "actions": [{
+                "action": "explore",
+                "title": "Go to the site"
+            }]
+        }
+    }
+
+
+    Promise.all(subscriptions.map(sub => webpush.sendNotification(
+        sub, JSON.stringify(notificationPayload))))
+        .then(() => res.status(200).json({ message: 'Newsletter sent successfully.' }))
+        .catch(err => {
+            console.error("Error sending notification, reason: ", err);
+            res.sendStatus(500);
+        });
+}
