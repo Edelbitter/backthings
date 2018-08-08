6432ca2eead35e285fd0cf39fabdfe05c6af2bfd
Regine Gorges <gorges.regine@gmail.com>
2018-08-08 12:10:58 +0200

    ich wuerd das commiten

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const url = require('url');
 
 app.get('/', (request, response) => response.send('Hiho'));
